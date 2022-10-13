0126461845c61de45a98d5925103327a8bd8ff38
Miguel Angel Mendigano Arismendy <105214371+mend1001@users.noreply.github.com>
2022-10-13 10:15:39 -0500

    the functionalities of the search pipe for the entity are created vehicle, power components are created

--- ASD-web/client/src/app/pipes/search-vehicle.pipe.ts
+++ ASD-web/client/src/app/pipes/search-vehicle.pipe.ts
@@ -10,7 +10,7 @@ export class SearchVehiclePipe implements PipeTransform {
     if (arg === '' || arg.length < 1) return value;
     const result = [];
     for (const vehicle of value) {
-      if (vehicle.mehnom.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
+      if (vehicle.vehnom.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         result.push(vehicle);
       };
     };
